82d11321d6d1269b46a2a845f3a00d1d3200dfce
rcammack <rcammack@hawaii.edu>
2018-03-05 20:25:40 -1000

    Completed islandsnow mockup

--- app/imports/ui/pages/IslandSnow.jsx
+++ app/imports/ui/pages/IslandSnow.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import TopMenu from '../components/TopMenu';
 import IslandSnowLogo from '../components/IslandSnowLogo';
 import MiddleMenu from '../components/MiddleMenu';
 import FullWidthImage from '../components/FullWidthImage';
-import FooterMenu from '../components/FooterMenu';
 
 export default class IslandSnow extends React.Component {
 
